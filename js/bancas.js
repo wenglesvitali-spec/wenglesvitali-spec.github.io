b59d01807fb08016e862de3433f1d63f6aee4bc5
revalida <wenglesvitali@gmail.com>
2026-05-22 23:18:51 -0400

    ss

---
+++ js/bancas.js
@@ -0,0 +1,11 @@
+/* ==================================================
+ABRIR E FECHAR BANCA
+================================================== */
+
+function toggleBanca(id){
+
+    let banca = document.getElementById(id);
+
+    banca.classList.toggle("aberta");
+
+}
